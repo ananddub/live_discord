26f8939f9536443a112d46cd23ea4f848277d07d
ananddub <duanand6@gmail.com>
2025-03-19 17:07:46 +0530

    add user create with nat service

--- src/models/user.modal.ts
+++ src/models/user.modal.ts
@@ -1,1 +1,94 @@
+import mongoose from "mongoose";
 
+const UserSchema = new mongoose.Schema(
+    {
+        _id: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        name: {
+            required: true,
+            type: String,
+        },
+        bio: {
+            type: String,
+            default: "",
+        },
+        friends: [
+            {
+                type: String,
+                ref: "User",
+            },
+        ],
+        pending: [
+            {
+                type: String,
+                ref: "User",
+            },
+        ],
+        reject: [
+            {
+                type: String,
+                ref: "User",
+            },
+        ],
+        block: [
+            {
+                type: String,
+                ref: "User",
+            },
+        ],
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const uniqueArrayValidator = function (arr) {
+    return arr.length === new Set(arr).size;
+};
+
+UserSchema.path("friends").validate(
+    uniqueArrayValidator,
+    "Duplicate friend IDs not allowed"
+);
+
+UserSchema.path("pending").validate(
+    uniqueArrayValidator,
+    "Duplicate pending IDs not allowed"
+);
+
+UserSchema.path("reject").validate(
+    uniqueArrayValidator,
+    "Duplicate reject IDs not allowed"
+);
+
+UserSchema.path("block").validate(
+    uniqueArrayValidator,
+    "Duplicate block IDs not allowed"
+);
+
+UserSchema.pre("save", function (next) {
+    const allIds = [
+        ...this.friends,
+        ...this.pending,
+        ...this.reject,
+        ...this.block,
+    ];
+
+    const totalLength = allIds.length;
+    const uniqueLength = new Set(allIds).size;
+
+    if (totalLength !== uniqueLength) {
+        return next(
+            new Error(
+                "Same user cannot be in multiple lists (friends/pending/reject/block)"
+            )
+        );
+    }
+
+    next();
+});
+
+export const User = mongoose.model("User", UserSchema);
